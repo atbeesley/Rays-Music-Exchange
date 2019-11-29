53b4f716d49eb7e8897f619108353242eee63cb3
user <you@exampalex.com>
2019-11-29 14:26:07 +0000

    trumpet tests passing

--- src/test/java/TrumpetTest.java
+++ src/test/java/TrumpetTest.java
@@ -1,2 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+import shop.instruments.Trumpet;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class TrumpetTest {
+
+    Trumpet trumpet;
+
+    @Before
+    public void before(){
+    trumpet = new Trumpet(3, "brass", "gold", "normal");
+    }
+
+    @Test
+    public void hasValves(){
+       assertEquals(3, trumpet.getValves());
+    }
+
+    @Test
+    public void hasMaterial(){
+        assertEquals("brass", trumpet.getMaterial());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("gold", trumpet.getColour());
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("normal", trumpet.getType());
+    }
+
+    @Test
+    public void canPlay(){
+        assertEquals("So What", trumpet.play());
+    }
+
+
 }
